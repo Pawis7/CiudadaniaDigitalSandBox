1e765b55a99c3229c03c6d5757203e52f7bf226f
emmanuel56mixta-ctrl <emmanuel56mixta@gmail.com>
2026-09-16 23:58:27 -0600

    feat: route unified resources catalog

--- CDJ-Front/src/app/app.routes.ts
+++ CDJ-Front/src/app/app.routes.ts
@@ -1,15 +1,10 @@
 import { Routes } from '@angular/router';
-export const routes: Routes = [
- {path:'',loadComponent:()=>import('./inicio/inicio').then(m=>m.InicioComponent),title:'Inicio · Ciudadanía Digital'},
- {path:'series',loadComponent:()=>import('./series/series-list/series-list').then(m=>m.SeriesListComponent),title:'Series · Ciudadanía Digital'},
- {path:'series/:slug',loadComponent:()=>import('./series/series-detail/series-detail').then(m=>m.SeriesDetailComponent),title:'Serie · Ciudadanía Digital'},
- {path:'edutips',loadComponent:()=>import('./edutips/edutips').then(m=>m.EdutipsComponent),title:'Edutips · Ciudadanía Digital'},
- {path:'el-dia-que-casi',loadComponent:()=>import('./casi-learning/casi-learning').then(m=>m.CasiLearningComponent),title:'El día que casi · Ciudadanía Digital'},
- {path:'muestra-edutip',redirectTo:'actividad/fomo',pathMatch:'full'},
- {path:'actividad/:slug',loadComponent:()=>import('./learning-experience/learning-experience').then(m=>m.LearningExperienceComponent),title:'Experiencia · Ciudadanía Digital'},
- {path:'quienes-somos',loadComponent:()=>import('./quienes-somos/quienes-somos').then(m=>m.QuienesSomosComponent),title:'Quiénes somos · Ciudadanía Digital'},
- {path:'pantallas-seguras',loadComponent:()=>import('./pantallas-seguras/pantallas-seguras').then(m=>m.PantallasSegurasComponent),title:'Pantallas Seguras · Ciudadanía Digital'},
- {path:'ayuda',loadComponent:()=>import('./ayuda/ayuda').then(m=>m.AyudaComponent),title:'Ayuda Digital · Ciudadanía Digital'},
- {path:'p/:slug',loadComponent:()=>import('./audiencia/audiencia').then(m=>m.AudienciaComponent),title:'Ciudadanía Digital'},
- {path:'**',redirectTo:''}
-];
+export const routes:Routes[]=[
+{path:'',loadComponent:()=>import('./inicio/inicio').then(m=>m.InicioComponent),title:'Inicio · Ciudadanía Digital'},
+{path:'recursos',loadComponent:()=>import('./recursos/recursos').then(m=>m.RecursosComponent),title:'Recursos · Ciudadanía Digital'},
+{path:'series',loadComponent:()=>import('./series/series-list/series-list').then(m=>m.SeriesListComponent),title:'Series · Ciudadanía Digital'},
+{path:'series/:slug',loadComponent:()=>import('./series/series-detail/series-detail').then(m=>m.SeriesDetailComponent),title:'Serie · Ciudadanía Digital'},
+{path:'edutips',loadComponent:()=>import('./edutips/edutips').then(m=>m.EdutipsComponent),title:'Edutips · Ciudadanía Digital'},
+{path:'el-dia-que-casi',loadComponent:()=>import('./casi-learning/casi-learning').then(m=>m.CasiLearningComponent),title:'El día que casi · Ciudadanía Digital'},
+{path:'muestra-edutip',redirectTo:'actividad/fomo',pathMatch:'full'},{path:'actividad/:slug',loadComponent:()=>import('./learning-experience/learning-experience').then(m=>m.LearningExperienceComponent),title:'Experiencia · Ciudadanía Digital'},
+{path:'quienes-somos',loadComponent:()=>import('./quienes-somos/quienes-somos').then(m=>m.QuienesSomosComponent),title:'Quiénes somos · Ciudadanía Digital'},{path:'pantallas-seguras',loadComponent:()=>import('./pantallas-seguras/pantallas-seguras').then(m=>m.PantallasSegurasComponent),title:'Pantallas Seguras · Ciudadanía Digital'},{path:'ayuda',loadComponent:()=>import('./ayuda/ayuda').then(m=>m.AyudaComponent),title:'Ayuda · Ciudadanía Digital'},{path:'p/:slug',loadComponent:()=>import('./audiencia/audiencia').then(m=>m.AudienciaComponent),title:'Ciudadanía Digital'},{path:'**',redirectTo:''}];
